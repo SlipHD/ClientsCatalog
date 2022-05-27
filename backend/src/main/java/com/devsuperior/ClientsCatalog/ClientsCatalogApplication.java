48024a3ef520eb7cd9864ad7b321a66cbef31d3a
Slip <zuraffa2015@gmail.com>
2022-05-27 19:48:00 -0300

    Backend, project created

---
+++ backend/src/main/java/com/devsuperior/ClientsCatalog/ClientsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ClientsCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsCatalogApplication.class, args);
+	}
+
+}
