d2a0dbf2b88a7932f68f7b3984b635c935e231e9
SlipHD <zuraffa2015@gmail.com>
2022-06-02 03:18:28 -0300

    H2 database, database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into tb_client(name, birth_date, children, cpf, income) values ('Felipe', TIMESTAMP WITH TIME ZONE '2000-07-13T15:50:00Z', 2, '13423132478', 3500.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Julio', TIMESTAMP WITH TIME ZONE '1988-11-04T00:47:00Z', 2, '23413246576', 2810.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Tania', TIMESTAMP WITH TIME ZONE '1976-06-07T12:23:30Z', 4, '16725434544', 4500.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Celso', TIMESTAMP WITH TIME ZONE '1989-07-01T07:15:00Z', 1, '26514526634', 2280.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Cristina', TIMESTAMP WITH TIME ZONE '2002-08-08T03:02:45Z', 0, '25678643171', 1800.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Ambrosio', TIMESTAMP WITH TIME ZONE '2000-02-15T12:50:40Z', 2, '29138257452', 5600.0);
+insert into tb_client(name, birth_date, children, cpf, income) values ('Juliana', TIMESTAMP WITH TIME ZONE '1999-04-13T15:20:00Z', 1, '27439527393', 3300.0);
